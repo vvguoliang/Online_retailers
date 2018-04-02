725d2d0e81cd3a7e1a95760a82f299265cbb0d4b
vvguoliang <qin.guoliang@shaxiaoseng.com>
2018-04-03 01:06:29 +0800

    双重viewpaget添加完成

--- wexpt/app/src/main/java/net/wexpt/com/wexpt/ui/MainTabFragment.java
+++ wexpt/app/src/main/java/net/wexpt/com/wexpt/ui/MainTabFragment.java
@@ -3,12 +3,20 @@ package net.wexpt.com.wexpt.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import net.wexpt.com.wexpt.R;
 import net.wexpt.com.wexpt.base.BaseFragment;
+import net.wexpt.com.wexpt.ui.viwepage.BannerViewHolder;
+import net.wexpt.com.wexpt.ui.viwepage.MZBannerView;
+import net.wexpt.com.wexpt.ui.viwepage.holder.MZHolderCreator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Time : 2018/4/2 no 下午6:48
@@ -31,17 +39,70 @@ import net.wexpt.com.wexpt.base.BaseFragment;
  * ******┗┻┛  ┗┻┛
  */
 
-public class MainTabFragment extends BaseFragment {
+public class MainTabFragment extends BaseFragment implements MZBannerView.BannerPageClickListener,ViewPager.OnPageChangeListener{
+
+    private MZBannerView banner;
+    public static final int []BANNER = new int[]{R.mipmap.banner1,R.mipmap.banner2,R.mipmap.banner3,R.mipmap.banner4,R.mipmap.banner5};
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fra_maintab, container, false);
+        banner = view.findViewById(R.id.banner);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        banner.addPageChangeLisnter(this);
+        banner.setBannerPageClickListener(this);
+
+        List<Integer> bannerList = new ArrayList<>();
+        for(int i=0;i<BANNER.length;i++){
+            bannerList.add(BANNER[i]);
+        }
+        banner.setIndicatorVisible(true);
+        // 代码中更改indicator 的位置
+        //mMZBanner.setIndicatorAlign(MZBannerView.IndicatorAlign.LEFT);
+        //mMZBanner.setIndicatorPadding(10,0,0,150);
+        banner.setPages(bannerList, new MZHolderCreator<BannerViewHolder>() {
+            @Override
+            public BannerViewHolder createViewHolder() {
+                return new BannerViewHolder();
+            }
+        });
+    }
+
+    @Override
+    public void onPageClick(View view, int position) {
+        Toast.makeText(getContext(),"click page:"+position,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        banner.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        banner.pause();
     }
 }
---
+++ wexpt/app/src/main/java/net/wexpt/com/wexpt/ui/viwepage/BannerViewHolder.java
@@ -0,0 +1,48 @@
+package net.wexpt.com.wexpt.ui.viwepage;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import net.wexpt.com.wexpt.R;
+import net.wexpt.com.wexpt.ui.viwepage.holder.MZViewHolder;
+
+/**
+ * @Time : 2018/4/3 no 上午12:49
+ * @USER : vvguoliang
+ * @File : BannerViewHolder.java
+ * @Software: Android Studio
+ * code is far away from bugs with the god animal protecting
+ * I love animals. They taste delicious.
+ * ***┏┓   ┏ ┓
+ * **┏┛┻━━━┛ ┻┓
+ * **┃   ☃   ┃
+ * **┃ ┳┛  ┗┳ ┃
+ * **┃    ┻   ┃
+ * **┗━┓    ┏━┛
+ * ****┃    ┗━━━┓
+ * ****┃ 神兽保佑 ┣┓
+ * ****┃ 永无BUG！┏┛
+ * ****┗┓┓┏━┳┓┏┛┏┛
+ * ******┃┫┫  ┃┫┫
+ * ******┗┻┛  ┗┻┛
+ */
+
+public class BannerViewHolder implements MZViewHolder<Integer> {
+
+    private ImageView mImageView;
+    @Override
+    public View createView(Context context) {
+        // 返回页面布局文件
+        View view = LayoutInflater.from(context).inflate(R.layout.banner_item,null);
+        mImageView = view.findViewById(R.id.banner_image);
+        return view;
+    }
+
+    @Override
+    public void onBind(Context context, int position, Integer data) {
+        // 数据绑定
+        mImageView.setImageResource(data);
+    }
+}
